3dc0c7b26d0402320a20123929e5a84f7915bf68
Michinobu Masayama <masayama.michinobu@ditgroup.jp>
2023-11-14 16:17:32 +0900

    ADD:四角形

--- src/BaseMap.tsx
+++ src/BaseMap.tsx
@@ -2,6 +2,10 @@ import { useEffect, useRef } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Polygon from "ol/geom/Polygon";
 import OSM from "ol/source/OSM";
 
 export const BaseMap = () => {
@@ -9,16 +13,35 @@ export const BaseMap = () => {
 
   useEffect(() => {
     if (mapRef.current) {
+      // 四角形の特徴を作成
+      const squareFeature = new Feature({
+        geometry: new Polygon([
+          [
+            [15090000, 4130000],
+            [15095000, 4130000],
+            [15095000, 4125000],
+            [15090000, 4125000],
+          ],
+        ]),
+      });
+
+      const vectorLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [squareFeature],
+        }),
+      });
+
       new Map({
         target: mapRef.current,
         layers: [
           new TileLayer({
             source: new OSM(),
           }),
+          vectorLayer,
         ],
         view: new View({
-          center: [0, 0],
-          zoom: 2,
+          center: [15100000, 4125000],
+          zoom: 12,
         }),
       });
     }
