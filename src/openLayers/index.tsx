251357160aabefce7a27fbdc7417fcda4a06d7ae
Michinobu Masayama <masayama.michinobu@ditgroup.jp>
2023-11-16 17:31:42 +0900

    REFACTOR:CSSファイル切り分け

--- src/openLayers/index.tsx
+++ src/openLayers/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
@@ -11,7 +12,9 @@ import DragBox from "ol/interaction/DragBox";
 import { shiftKeyOnly, platformModifierKeyOnly } from "ol/events/condition";
 import { Polygon } from "ol/geom";
 import { Feature } from "ol";
+
 import { useBaseMap } from "./hooks";
+import "./styles.css";
 
 export const BaseMap = () => {
   const { features } = useBaseMap();
